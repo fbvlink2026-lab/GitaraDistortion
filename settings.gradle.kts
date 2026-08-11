4ee0e34e9d990f5d0ab4b84203d5e5622244f47b
fbvlink2026-lab <sunato2026@gmail.com>
2026-08-12 01:59:51 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.5.0"
+        id("com.android.application") version "8.2.0"
         id("org.jetbrains.kotlin.android") version "1.9.20"
     }
 }
